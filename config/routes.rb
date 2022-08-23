7c14c97e64b2cb982d6f8a6362557959a1a52ba4
Patrick Misner <misner.patrick@gmail.com>
2022-08-23 01:21:00 -0600

    password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   #  get to /about
   get "about-us", to: "about#index", as: :about
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
   root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
