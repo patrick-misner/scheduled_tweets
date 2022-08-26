992d875bf0a007e3686a8cdcfbc2b2e4583e3a3c
Patrick Misner <misner.patrick@gmail.com>
2022-08-26 02:19:49 -0600

    crud methods for tweets

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get "/auth/twitter2/callback", to: "omniauth_callbacks#twitter2"
 
   resources :twitter_accounts
+  resources :tweets
 
 
   root to: "main#index"
